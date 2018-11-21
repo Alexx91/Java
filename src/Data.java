6e1e0757efc5620dba9e7cdb5f4c60905388423d
Alexx91 <a.mihaita91@yahoo.com>
2018-11-21 13:07:11 +0200

    import

---
+++ src/Data.java
@@ -0,0 +1,16 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 27;
+        float weight = 83.4f;
+        double height = 1.85;
+        long distance = 3325642434l;
+        char gender = 'm';
+        char test = 9;
+        boolean isStudent = false;
+        boolean hasJob = true;
+        int test2 = (int) weight;
+        System.out.println(test);
+        System.out.println(gender);
+        System.out.println(test2);
+    }
+}

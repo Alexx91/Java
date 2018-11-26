0cda09929aa3768a925239615a2da87601d2c53d
Alexx91 <a.mihaita91@yahoo.com>
2018-11-26 17:03:05 +0200

    ghiceste numarul

--- src/YourNumber.java
+++ src/YourNumber.java
@@ -4,39 +4,45 @@ public class YourNumber {
     public static void main(String[] args) {
 
         System.out.print("Care e numarul maxim? : ");
-        Scanner max = new Scanner(System.in);
-        int z = max.nextInt();
+
+        Scanner z = new Scanner(System.in);
+        int max = z.nextInt();
 
         System.out.print("Cate incercari? : ");
 
         Scanner tries = new Scanner(System.in);
-        int y = tries.nextInt();
-        int i = y;
+        int c = tries.nextInt();
 
-        int randomNumber = (int) (Math.random() * z);
+        int i = 0;
+        String again;
 
-        for (y = 0; y < i; y++) {
-            Scanner number = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * max);
+        do {
+            while (i < c) {
+                Scanner number = new Scanner(System.in);
 
-            System.out.print("Care este numarul? : ");
+                System.out.print("Care este numarul magic? : ");
 
-            int x = number.nextInt();
+                int x = number.nextInt();
 
-            if (x > randomNumber ) {
-                System.out.println("mai mic");
-
-            } else if (x < randomNumber){
-                System.out.println("mai mare");
-            } else if (x == randomNumber){
-                System.out.println("ai castigat");
-                break;
-            } else {
-                System.out.println("ai pierdut");
+                if (x != randomNumber && i == c - 1) {
+                    System.out.println("Ai pierdut :(");
+                    break;
+                } else if (x > randomNumber) {
+                    System.out.println("Mai mic");
+                } else if (x < randomNumber) {
+                    System.out.println("Mai mare");
+                } else {
+                    System.out.println("Ai castigat!!!");
+                    break;
+                }
+                i++;
             }
-
-        }
-
-
-
+            System.out.println("Mai incercam odata? Y/N");
+            Scanner answer = new Scanner(System.in);
+            again = answer.nextLine();
+            i = 0;
+        } while (again.equalsIgnoreCase("y"));
+        System.out.println("Pe data viitoare!");
     }
 }

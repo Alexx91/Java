fbe869109b92c2a6493675f019dae937d4428c06
Alexx91 <a.mihaita91@yahoo.com>
2018-11-26 22:54:53 +0200

    my generation

--- src/Generation.java
+++ src/Generation.java
@@ -1,17 +1,22 @@
+import java.util.Scanner;
+
 public class Generation {
     public static void main(String[] args) {
-        int age = 27;
-        if (age < 6) {
+
+        Scanner age = new Scanner(System.in);
+        System.out.print("Your age : ");
+         int x = age.nextInt();
+        if (x < 6) {
             System.out.println("I am Generation Alpha");
-        } else if(age >=6 && age <=20) {
+        } else if(x >=6 && x <=20) {
             System.out.println("I am Generation Z");
-        } else if(age >=21 && age <=35) {
+        } else if(x >=21 && x <=35) {
             System.out.println("I am Generation Y");
-        } else if(age >=36 && age <=50) {
+        } else if(x >=36 && x <=50) {
             System.out.println("I am Generation X");
-        } else if(age >=51 && age <=69) {
+        } else if(x >=51 && x <=69) {
             System.out.println("I am Baby Boomer");
-        } else if(age >=70) {
+        } else if(x >=70) {
             System.out.println("I am Builder");
         }
     }
